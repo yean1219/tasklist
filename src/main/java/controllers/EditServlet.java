52f1363ccc55d927b85fd0c9feb1140a78edccec
yean1 <yean1@DESKTOP-9SUDEJ1>
2022-04-17 01:08:56 +0900

    最終確認

--- src/main/java/controllers/EditServlet.java
+++ src/main/java/controllers/EditServlet.java
@@ -50,8 +50,5 @@ public class EditServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
 }
--- src/main/java/controllers/ShowServlet.java
+++ src/main/java/controllers/ShowServlet.java
@@ -49,9 +49,5 @@ public class ShowServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
 
 }
