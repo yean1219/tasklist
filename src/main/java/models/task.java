bca82bf079770c85f5cea8ae5f8de1ea2db62f22
yean1 <yean1@DESKTOP-9SUDEJ1>
2022-04-16 23:00:21 +0900

    getAlltasksを追加

--- src/main/java/models/task.java
+++ src/main/java/models/task.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAlltasks",
+        query = "SELECT m FROM task AS m ORDER BY m.ID DESC"
+    )
+})
 @Table(name = "tasks")
 public class task {
     @Id
